2cf977a268399ef95c69c7e7ac36bec4c24d97d4
Megha619801 <meghak@metropolia.fi>
2024-12-01 21:29:09 +0200

    Add files via upload

---
+++ module3/t3/3.js
@@ -0,0 +1,6 @@
+'use strict';
+const names = ['John', 'Paul', 'Jones'];
+
+let target = document.getElementById("target");
+for (let i = 0; i < names.length; i++)
+  target.innerHTML += "<li>" + names[i] + "</li>";
